d4e0275dd74693019d5468ed7e325c4178a41640
cmendoza <clrwxb@gmail.com>
2021-03-11 17:33:54 -0800

    coded mice indexroute, coded mice index view, created mouse class, created mice list, created mouse index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path("", views.home, name="home"),
   path("about/", views.about, name="about"),
+  path("mice/", views.mice_index, name="index"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+# create the mouse class 
+class Mouse:
+  def __init__(self, name, species, description, age):
+    self.name = name
+    self.species = species
+    self.description = description
+    self.age = age
+
+mice = [
+  Mouse("Eek", "white mouse", "chubby yet fast white mouse with brown spots", 2),
+  Mouse("Phoebe", "house mouse", "loud and likes to run around", 1),
+  Mouse("Cheeks", "deer mouse", "small and quiet", 4)
+]
+
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Mouse! /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
   return render(request, "about.html")
+
+def mice_index(request):
+  return render(request, "mice/index.html", { "mice": mice })
